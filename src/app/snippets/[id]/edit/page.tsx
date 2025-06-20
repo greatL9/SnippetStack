bd3a86690385da564b6dd46f9c3505783668eadd
Great Lucky <greatlucky@G.local>
2025-06-20 13:18:10 +0100

    Initial commit

---
+++ src/app/snippets/[id]/edit/page.tsx
@@ -0,0 +1,26 @@
+import { notFound } from "next/navigation";
+import { db } from "@/db";
+import SnippetEditForm from "@/components/snippet-edit-form";
+
+interface SnippetEditPageProps {
+  params: {
+    id: string;
+  };
+}
+
+export default async function EditSnippetPage(props: SnippetEditPageProps) {
+  const id = parseInt(props.params.id);
+  const snippet = await db.snippet.findFirst({
+    where: {
+      id,
+    },
+  });
+  if (!snippet) {
+    notFound();
+  }
+  return (
+    <div>
+      <SnippetEditForm snippet={snippet} />
+    </div>
+  );
+}
---
+++ src/app/snippets/[id]/not-found.tsx
@@ -0,0 +1,7 @@
+export default function SnippetNotFound() {
+  return (
+    <div className="text-xl bold">
+      <h1>Sorry, but we could not find that particular snippet</h1>
+    </div>
+  );
+}
